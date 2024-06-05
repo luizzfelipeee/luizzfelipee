d38e32ef497c2e537fd0a2ddd2c97ae6ff018ee5
luizzfelipeee <00001093482928SP@al.educacao.sp.gov.br>
2024-06-05 09:50:24 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+
+  if (mouseY > posicaoVertical) {
+    posicaoVertical++;
+  }
+
+  if (mouseIsPressed) {
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
